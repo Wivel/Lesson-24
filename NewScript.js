cd57704f37b6101001c0bc69589ebb95c98c323e
Wivel <wiveloriginal@yandex.ru>
2018-10-28 18:56:31 +0300

    12 point

--- NewScript.js
+++ NewScript.js
@@ -1,6 +1,12 @@
-function adder() {
-    var ol = document.getElementById('ol');
-    var li = document.createElement('li');
-    li.innerHTML = 'пункт';
-    ol.appendChild(li);
+function add_elem() {
+let ul      = document.getElementById('ul'),
+    number  = document.getElementById('number_input'),
+    str     = '';
+    for (let i = 1; i <= number.value; i++) {
+        let li = document.createElement('li');
+        str = i;
+        li.innerHTML = str;
+        ul.appendChild(li);
+    }
+
 }
