9cc9db355a79200f7b31eeba232bc47ffe66c356
Wivel <wiveloriginal@yandex.ru>
2018-10-28 17:17:13 +0300

    10 point

--- NewScript.js
+++ NewScript.js
@@ -1,52 +1,12 @@
-function doTask() {
+function showTEG() {
     let elem = document.getElementById('elem');
-    elem.classList.add('www');
-    Point_3();
+    alert(elem.tagName);
 }
 
-function doTask2() {
-    let elem = document.getElementById('elem');
-    elem.classList.remove('www');
-    Point_3();
-}
-
-function Point_3() {
-    let elem = document.getElementById('elem');
-    if (elem.classList.contains('www')) {
-        elem.innerHTML = 'у elem есть класс www!';
+function addTEG() {
+    let elems = document.querySelectorAll('.www');
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].innerHTML = elems[i].innerHTML + '(' + elems[i].tagName[0]+ elems[i].tagName.substr(1).toLowerCase()+ ')';
     }
-    else {
-        elem.innerHTML = 'у elem отсутвует класс www!';
-    }
-}
 
-function Point_4() {
-    let elem = document.getElementById('elem');
-    elem.classList.toggle('www');
-    Point_3();
-}
-
-function Point_5() {
-    let elem = document.getElementById('elem');
-    elem.innerHTML = 'Количество классов объекта elem - ' + elem.classList.length;
-}
-
-function Point_6() {
-    let
-        elem        = document.getElementById('elem'),
-        classlist   = elem.classList;
-
-    if (classlist.length == 0) {
-        alert('Классов у объекта - нет');
-    }
-    else {
-        for (let i = 0; i < classlist.length; i++) {
-            alert(classlist[i]);
-        }
-    }
-}
-
-function add_CSS() {
-    let elem        = document.getElementById('elem');
-    elem.style.cssText = 'color: red; font-size: 30px; border: none';
 }
