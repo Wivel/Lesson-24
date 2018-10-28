92e2471090a807b78f036dec1a85adbd40a0b13c
Wivel <wiveloriginal@yandex.ru>
2018-10-28 18:24:35 +0300

    11 point

--- NewScript.js
+++ NewScript.js
@@ -1,12 +1,6 @@
-function showTEG() {
-    let elem = document.getElementById('elem');
-    alert(elem.tagName);
-}
-
-function addTEG() {
-    let elems = document.querySelectorAll('.www');
-    for (let i = 0; i < elems.length; i++) {
-        elems[i].innerHTML = elems[i].innerHTML + '(' + elems[i].tagName[0]+ elems[i].tagName.substr(1).toLowerCase()+ ')';
-    }
-
+function adder() {
+    var ol = document.getElementById('ol');
+    var li = document.createElement('li');
+    li.innerHTML = 'пункт';
+    ol.appendChild(li);
 }
